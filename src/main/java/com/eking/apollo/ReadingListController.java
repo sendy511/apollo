d49206a860057adddd7d46b91eb949e6a470ab3d
Yansen Dou <sendy511@hotmail.com>
2017-08-03 15:33:56 +0800

    Add Books

--- src/main/java/com/eking/apollo/ReadingListController.java
+++ src/main/java/com/eking/apollo/ReadingListController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -24,5 +25,12 @@ public class ReadingListController {
         model.addAttribute("books", books);
 
         return "readingList";
+    }
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book){
+        book.setReader(reader);
+        this.readingListRepository.save(book);
+        return "redirect:/readingList/{reader}";
     }
 }
