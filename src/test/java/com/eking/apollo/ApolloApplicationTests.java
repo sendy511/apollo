c07b72fe4e36f1e5facaba3cdfba99a253302d45
Yansen Dou <sendy511@hotmail.com>
2017-08-10 11:10:34 +0800

    test get reading list response is ok

--- src/test/java/com/eking/apollo/ApolloApplicationTests.java
+++ src/test/java/com/eking/apollo/ApolloApplicationTests.java
@@ -1,16 +1,33 @@
 package com.eking.apollo;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApolloApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
 
+    @Before
+    public void mockMvcBuild(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+	@Test
+	public void should_get_readinglist_ok() throws Exception {
+	    mockMvc.perform(MockMvcRequestBuilders.get("/readingList/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
