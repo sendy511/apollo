8dbcfb907b12fd0096d15c853cabb5db96259aa7
Yansen Dou <sendy511@hotmail.com>
2017-08-10 14:02:45 +0800

    Add test to verify the add new book to reading list

--- src/test/java/com/eking/apollo/ApolloApplicationTests.java
+++ src/test/java/com/eking/apollo/ApolloApplicationTests.java
@@ -1,17 +1,19 @@
 package com.eking.apollo;
 
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -34,6 +36,26 @@ public class ApolloApplicationTests {
                 .andExpect(status().isOk())
                 .andExpect(view().name("readingList"))
                 .andExpect(model().attributeExists("books"))
-                .andExpect(model().attribute("books", Matchers.is(Matchers.empty())));
+                .andExpect(model().attribute("books", is(empty())));
 	}
+
+    @Test
+    public void should_be_able_to_add_book_to_reading_list() throws Exception {
+        String bookTitle = "a new book";
+        String readerId = "1";
+        mockMvc.perform(post("/readingList/" + readerId)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("title", bookTitle))
+                .andExpect(status().is3xxRedirection());
+
+        Book expectedBook = new Book();
+        expectedBook.setId(1L);
+        expectedBook.setTitle(bookTitle);
+        expectedBook.setReader(readerId);
+
+        mockMvc.perform(get("/readingList/" + readerId))
+                .andExpect(model().attribute(
+                        "books",
+                        contains(samePropertyValuesAs(expectedBook))));
+    }
 }
